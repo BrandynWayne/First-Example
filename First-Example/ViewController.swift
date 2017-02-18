ee3944e7e620362c1ee53e6a9bc6a383b150649f
Brandyn Burgett <brandynburgett@Brandyns-MacBook-Air.local>
2017-02-18 00:57:01 -0600

    This is the second commit

---
+++ First-Example/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  First-Example
+//
+//  Created by Brandyn Burgett on 2/18/17.
+//  Copyright © 2017 Brandyn Burgett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Destroy The World!")
+    
+    }
+        
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
